c7cebdbc922d4fdb348799b3eaf93b4114ce9d5e
emptycoder <yaroslav200124@gmail.com>
2021-03-27 10:50:56 +0200

    Add squared and linear graphics

--- main.R
+++ main.R
@@ -7,16 +7,17 @@ source("methods.R")
 data <- read_xlsx("matrixAndVector.xlsx")
 # Preparing data
 vector <- as.vector(data$Vector)
-matrix <- as.matrix(data[,3:ncol(data)])
+vector2 <- as.vector(data$Vector2)
+
+matrix <- as.matrix(data[,3:6])
 matrix <- matrix[apply(matrix, 1, Compose(is.finite, all)),]
+matrix2 <- as.matrix(data[,10:13])
+matrix2 <- matrix2[apply(matrix2, 1, Compose(is.finite, all)),]
 # Matrix generation
 #write.csv(matrix(runif(60 * 1, min = 0, max = 1), ncol = 1, nrow = 60), file = "test1.csv")
 
 #h <- as.numeric(readline(prompt = "Enter h: "))
 #delta <- as.numeric(readline(prompt = "Enter delta: "))
-h <- 0.0001
-delta <- 0.0001
-eps <- 0.00001
 
 transponentMatrix <- t(matrix)
 # Multiply transponented matrix with general matrix
@@ -24,20 +25,32 @@ MTGmatrix <- transponentMatrix %*% matrix
 # Multiply transponented matrix with vector
 MTVcoefficients <- transponentMatrix %*% vector
 
-alpha1 <- 0.01
-alpha2 <- 0.1
+transponentMatrix2 <- t(matrix2)
+# Multiply transponented matrix with general matrix
+MTGmatrix2 <- transponentMatrix2 %*% matrix2
+# Multiply transponented matrix with vector
+MTVcoefficients2 <- transponentMatrix2 %*% vector2
 
 if (alpha1 > alpha2) {
   stop("alpha1 > alpha2")
 }
 
-x1 <- TihonovMethod(MTGmatrix, MTVcoefficients, alpha1, alpha2, delta, h, eps)
+x1 <- TihonovMethod(MTGmatrix, MTVcoefficients, alpha1 = 0.01, alpha2 = 0.1, delta = 0.0001, h = 0.0001, eps = 0.00001)
+x2 <- TihonovMethod(MTGmatrix2, MTVcoefficients2, alpha1 = 0.01, alpha2 = 0.1, delta = 0.0001, h = 0.0001, eps = 0.00001)
 
-graphicsData <- data.frame(matrix %*% x1, vector)
+graphicsData <- data.frame(vector, matrix %*% x1)
 colnames(graphicsData) <- c("x", "y")
+
+linearGraphicsData <- data.frame(vector2, matrix2 %*% x2)
+colnames(linearGraphicsData) <- c("x", "y")
 
 # Scatter diagram
 ggplot(data = graphicsData, mapping = aes(x = graphicsData$x, y = graphicsData$y)) +
   theme_light() +
-  geom_point(fill = "black", size = 1) +
-  geom_smooth()
+  geom_point(colour = "black", size = 1) +
+  geom_abline()
+
+ggplot(data = linearGraphicsData, mapping = aes(x = linearGraphicsData$x, y = linearGraphicsData$y)) +
+  theme_light() +
+  geom_point(colour = "red", size = 1) + 
+  geom_abline()
